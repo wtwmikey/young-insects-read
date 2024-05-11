5b138142e93e91fbdf59d3d90d7b4f65a12b1753
Mikey <218mikey@gmail.com>
2024-05-11 17:42:33 -0400

    final auth

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
+import {Home, ForgotPassword, Login, Register } from './pages';
+
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
@@ -36,6 +38,10 @@ function App() {
               >
                 <Routes>
                   <Route index element={<WelcomePage />} />
+                  <Route index element={<Home />} />
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
