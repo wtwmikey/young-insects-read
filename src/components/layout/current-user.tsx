22f495544a827b5b26455f3e9ddb60750d7aba98
Mikey <218mikey@gmail.com>
2024-05-11 22:01:19 -0400

    moreeeeee

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,7 +1,12 @@
 import { Popover, Button } from 'antd'
-import React from 'react'
+import CustomAvatar from '../custom-avatar'
+import { useGetIdentity } from '@refinedev/core'
 
-const CurrentUser = () => {
+import type { User } from '@/graphql/schema.types'
+
+const CurrentUser = () => { 
+  const {data: user} = useGetIdentity<User>()
+
   return (
     <>
         <Popover
@@ -10,7 +15,7 @@ const CurrentUser = () => {
         overlayInnerStyle={{padding: 0}}
         overlayStyle={{zIndex: 9}}    
     >
-        TEST
+        <CustomAvatar />
         </Popover>
     </>
   )
